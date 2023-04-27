5475574e5daed264eb9afd4292860e00a76f9393
Izbassar <izok2004@gmail.com>
2023-04-27 13:54:06 +0600

    adding all the methods and construct

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,34 @@
-public class MyHashTable<K,V> {
+import java.util.ArrayList;
 
+public class MyHashTable<K,V> {
+    private ArrayList<HashNode<K, V>> myArr;
+    private int index;
+    private int size;
+    private class HashNode<K, V>{
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K, V>[] chainArray; // or Object[]
+    public MyHashTable() {
+        myArr = new ArrayList<>();
+        size = 0;
+        index = 0;
+    }
+    public MyHashTable(int M) {}
+    private int hash(K key) {}
+    public void put(K key, V value) {}
+    public V get(K key) {return }
+    public boolean contains(V value) {}
+    public K getKey(V value) {}
 }
