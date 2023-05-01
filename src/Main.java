72bb9447ac9149960e988bce1b595b27dd22db15
Izbassar <izok2004@gmail.com>
2023-05-01 16:50:25 +0600

    Izbassar

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        MyHashTable<Testing, MobilePhone> table = new MyHashTable<>();
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++) {
+            Testing key = new Testing(rand.nextInt(10000));
+            MobilePhone value = new MobilePhone(rand.nextInt(100), rand.nextInt(100));
+            table.put(key, value);
+        }
+
 
     }
-
 }
